69719607fed6b78782cf649e271fd9d2de5630dd
kdong <kdong@uoguelph.ca>
2017-09-27 12:07:42 -0400

    Add light sensor and proximity sensor so that one SensorManager controls all three sensors. -- TODO: need to add a onPause() and onResume() mathods

--- app/src/main/java/com/example/engg6600/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/engg6600/accelerometer/MainActivity.java
@@ -13,41 +13,57 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     private TextView xField, yField, zField, lightField, proximityField;
 
-//    private SensorManager sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+        //not used
+    }
+
+    SensorManager sm;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+//            xField.setText("X: "+ event.values[0]);
+//            yField.setText("Y: "+ event.values[1]);
+//            zField.setText("Z: "+ event.values[2]);
+            String stringX = "X: "+ event.values[0];
+            String stringY = "Y: "+ event.values[1];
+            String stringZ = "Z: "+ event.values[2];
+            xField.setText(stringX);
+            yField.setText(stringY);
+            zField.setText(stringZ);
+        }
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            String stringLight = "Light:"+ event.values[0];
+            lightField.setText(stringLight);
+        }
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            String stringProximity = "Proximity:" + event.values[0];
+            proximityField.setText(stringProximity);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SensorManager sm = (SensorManager)getSystemService(SENSOR_SERVICE);
-//        Sensor mySensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        Sensor lightSensor = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
-        Sensor proximitySensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+//        SensorManager sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
 
-//        sm.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
-//        sm.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+		Sensor accelerometerSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		Sensor lightSensor = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+		Sensor proximitySensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+		sm.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
         sm.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-//        xField = (TextView)findViewById(R.id.textView);
-//        yField = (TextView)findViewById(R.id.textView2);
-//        zField = (TextView)findViewById(R.id.textView3);
-//        lightField = (TextView)findViewById(R.id.textView4);
+        xField = (TextView)findViewById(R.id.textView);
+        yField = (TextView)findViewById(R.id.textView2);
+        zField = (TextView)findViewById(R.id.textView3);
+        lightField = (TextView)findViewById(R.id.textView4);
         proximityField = (TextView)findViewById(R.id.textView5);
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy){
-        //not used
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event){
-//        xField.setText("X: "+ event.values[0]);
-//        yField.setText("Y: "+ event.values[1]);
-//        zField.setText("Z: "+ event.values[2]);
-//        lightField.setText("Light:"+ event.values[0]);
-        proximityField.setText("Proximity:"+ event.values[0]);
     }
 
 //    @Override
